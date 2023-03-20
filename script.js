c4ab4c3568dfe7013919dd79f374b81a5e4fa3b9
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-03-20 23:49:46 +0200

    Added implementation using static initialization block

--- script.js
+++ script.js
@@ -18,7 +18,27 @@ class MySQLDatabase {
     }
 }
 
+class MongoDBDatabase {
+    static #data;
+
+    // Static initialization block introduced from ES13
+    static {
+        this.#data = Math.random();
+    }
+
+    get data() {
+        return MongoDBDatabase.#data;
+    }
+}
+
+console.log("Obvious way:");
 const d1 = new MySQLDatabase();
 console.log(d1.data);
 const d2 = new MySQLDatabase();
 console.log(d2.data);
+
+console.log("\nStatic initialization block way:");
+const d3 = new MongoDBDatabase();
+console.log(d3.data);
+const d4 = new MongoDBDatabase();
+console.log(d3.data);
