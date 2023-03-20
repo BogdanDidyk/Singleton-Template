aead5eb0602dc0b4eb757d7fb198f5e480c67886
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-03-20 23:29:54 +0200

    Added singleton implementation

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Nothing here. For now.");
+class MySQLDatabase {
+    static #instance;
+    #data;
+
+    get data() {
+        return this.#data;
+    }
+
+    constructor() {
+        if (MySQLDatabase.#instance) return MySQLDatabase.#instance;
+
+        this.receiveData();
+        MySQLDatabase.#instance = this;
+    }
+
+    receiveData() {
+        this.#data = Math.random();
+    }
+}
+
+const d1 = new MySQLDatabase();
+console.log(d1.data);
+const d2 = new MySQLDatabase();
+console.log(d2.data);
